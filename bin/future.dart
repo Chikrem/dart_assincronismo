8b9cf3b5eb7bb6df2f2b249f7546f02f36efe982
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-11-21 20:25:18 -0300

    try/catch/finally

--- bin/future.dart
+++ bin/future.dart
@@ -1,17 +1,45 @@
 void main() {
+  // Criando um Future de forma direta, sem usar async/await
+  // Future myFuture = Future(() {
+  //   print('Going back to the Future');
+  //   return 21;  // Retorna um valor de 21, que será passado para o callback 'then'
+  // }).then((value) => print('The value is $value'));  // O método 'then' é chamado quando o Future é resolvido
 
-    Future myFuture = Future((){
-        print('Going back to the Future');
-        return 21;
-    }).then((value) => print('The value is $value'));
-        
-    Future<int> myFutureFunc() async {
-        print('I have a function in the Future');
-        await Future.delayed(Duration(seconds: 5));
-        return 12;
+  // Definindo uma função assíncrona que retorna um Future com valor inteiro
+  // Future<int> myFutureFunc() async {
+  //   print('I have a function in the Future');
+  //   await Future.delayed(Duration(seconds: 5));  // Simula uma operação assíncrona que leva 5 segundos
+  //   throw Exception();
+  //   return 12;  // Retorna o valor 12 após o delay
+  // }
+
+  // Chamando a função assíncrona 'myFutureFunc' e tratando seu retorno com 'then'
+    // myFutureFunc()
+    //     .then((value) =>print('My function value is: $value'))
+    //     .onError((error, stackTrace) => print('An Error occurred'))
+    //     .whenComplete(() => print('The Future is Over'));
+
+    Future<int> myFutureErrorFunc(int a, int b) async {
+    try{
+      if(b>a){
+        throw Exception();
+      }
+      print('I am a Functional Function');
+
+      await Future.delayed(Duration(seconds: 5));
+      return 42;
+
+    }catch(e){
+      print('An Error occurred: $e');
+      return 42;
+
+    }finally{
+      print('The Future is finally Over');
     }
 
-    myFutureFunc().then((value) =>print('My function value is: $value'));
+    }
 
-    print('Done with main');
+  myFutureErrorFunc(2, 1).then((value) => print('The value is: $value'));
+
+  print('Done with main');
 }

c3a4997c76a5e2814299b6eb83dfc7c624288f96
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-11-21 19:32:53 -0300

    Future().then

--- bin/future.dart
+++ bin/future.dart
@@ -1,9 +1,17 @@
 void main() {
-    
+
     Future myFuture = Future((){
         print('Going back to the Future');
         return 21;
-    });
-    
+    }).then((value) => print('The value is $value'));
+        
+    Future<int> myFutureFunc() async {
+        print('I have a function in the Future');
+        await Future.delayed(Duration(seconds: 5));
+        return 12;
+    }
+
+    myFutureFunc().then((value) =>print('My function value is: $value'));
+
     print('Done with main');
 }
